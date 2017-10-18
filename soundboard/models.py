f761a469ae9ef0070c80c4050a42d875cba49cab
Ryan Regan <reganenergy@gmail.com>
2017-10-18 19:09:11 -0400

    Soundsboard Model

--- soundboard/models.py
+++ soundboard/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Sound(models.Model):
     name = models.CharField(max_length=140)
+    text = models.CharField(max_length=280, null=True)
+    sound_file = models.FileField(upload_to='sounds/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_file = models.FileFiled(upload_to='sound_images/')
+
+class SoundBoard(models.Model):
+    title = models.CharField(max_length=140)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
