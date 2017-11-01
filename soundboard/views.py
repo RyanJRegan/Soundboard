6a584b1bd06dc2d3495067c579262def88d062e0
Ryan Regan <reganenergy@gmail.com>
2017-11-01 19:39:18 -0400

    completed basic login/signup form

--- soundboard/views.py
+++ soundboard/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
 
 def index(request):
     return render(request, "soundboard/index.html")
@@ -13,8 +22,7 @@ def login_user(request):
         return #TODO error message city
     authentication_form = AuthenticationForm(data=request.POST)
     if not authentication_form.is_valid():
-        return render(request, "soundboard/login_or_signup.html", {'authentication_form': authentication_form})
-    # authentication_form.save()
+        return login_or_signup(request, authentication_form=authentication_form)
     username = authentication_form.cleaned_data.get('username')
     password = authentication_form.cleaned_data.get('password')
     user = authenticate(username=username, password=password)
@@ -25,9 +33,24 @@ def login_user(request):
 def your_boards(request):
     return render(request, "soundboard/your_boards.html")
 
+@csrf_protect
 def signup(request):
-   pass
+    if request.method != 'POST':
+        return #TODO error message city
+    user_creation_form = SignUpForm(data=request.POST)
+    if not user_creation_form.is_valid():
+        return login_or_signup(request, user_creation_form=user_creation_form)
+    user_creation_form.save()
+    username = user_creation_form.cleaned_data.get('username')
+    password = user_creation_form.cleaned_data.get('password1')
+    user = authenticate(username=username, password=password)
+    login(request, user)
+    return redirect('your_boards')
 
-def login_or_signup(request):
-    authentication_form = AuthenticationForm()
-    return render(request, "soundboard/login_or_signup.html", {'authentication_form': authentication_form})
+def login_or_signup(request, authentication_form=None, user_creation_form=None):
+    if authentication_form is None:
+        authentication_form = AuthenticationForm()
+    if user_creation_form is None:
+        user_creation_form = SignUpForm()
+    return render(request, "soundboard/login_or_signup.html", {'authentication_form': authentication_form,
+        'user_creation_form': user_creation_form})
