ab3a69fc47e84447ae5074e316d0dadc76f314c6
Ryan Regan <reganenergy@gmail.com>
2017-11-06 18:39:53 -0500

    signup test cases

--- soundboard/tests.py
+++ soundboard/tests.py
@@ -1,6 +1,27 @@
-from django.test import TestCase
-print("heyy")
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 class AuthenticationTestCase(TestCase):
-    def test_signup(self):
-        self.assertEqual(True, False)
+    def test_signup_creates_user(self):
+        client = Client()
+        client.post('/signup/', {
+            'username': 'ryguy',
+            'email': 'ryguy@example.com',
+            'password1': 'mtxMAPC6ch1EP',
+            'password2': 'mtxMAPC6ch1EP',
+        })
+
+        new_user = User.objects.get(username='ryguy')
+
+        self.assertEqual(new_user.email, 'ryguy@example.com')
+
+    def test_signup_redirects_to_your_boards(self):
+        client = Client()
+        response = client.post('/signup/', {
+            'username': 'ryguy',
+            'email': 'ryguy@example.com',
+            'password1': 'mtxMAPC6ch1EP',
+            'password2': 'mtxMAPC6ch1EP',
+        })
+
+        self.assertEqual(response.url, '/your_boards/')
