809397212d64e5ac0023090ec4c2b0c605217200
Ryan Regan <reganenergy@gmail.com>
2017-12-04 21:37:54 -0500

    added associateion model for soundboards and sounds

--- soundboard/models.py
+++ soundboard/models.py
@@ -7,7 +7,7 @@ class Sound(models.Model):
     text = models.CharField(max_length=280, null=True)
     sound_file = models.FileField(upload_to='sounds/')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image_file = models.FileField(upload_to='sound_images/', blank=True)
+    image_file = models.FileField(upload_to='sound_images/', blank=True, null=True)
 
     def as_dict(self):
         if self.image_file != '':
@@ -23,6 +23,25 @@ class Sound(models.Model):
             'sound_file': '/media/' + self.sound_file.url,
         }
 
-# class SoundBoard(models.Model):
-#     name = models.CharField(max_length=140)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class Soundboard(models.Model):
+    name = models.CharField(max_length=140)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sounds = models.ManyToManyField(
+             Sound,
+             through='SoundboardAssociation',
+             through_fields=('soundboard', 'sound'),
+             )
+
+    def __str__(self):
+         return self.name
+
+class SoundboardAssociation(models.Model):
+    sound = models.ForeignKey(Sound, on_delete=models.CASCADE)
+    soundboard = models.ForeignKey(Soundboard, on_delete=models.CASCADE)
+    order = models.IntegerField(),
+
+    def __str__(self):
+        return self.name
